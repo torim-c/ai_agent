3312d77d16139ac836e790372e41fece800616b5
torim-c <mocanu.v.cezar@gmail.com>
2026-09-19 04:39:00 +0300

    B: Exercise progress

--- main.py
+++ main.py
@@ -22,17 +22,21 @@ def main():
 
     parser = argparse.ArgumentParser(description="user prompt to llm")
     parser.add_argument("user_prompt", type=str, help="User prompt argument after uv run main.py, enclose in double quotes")
+    parser.add_argument('--verbose', action='store_true', help ="Enable verbose output")
     args = parser.parse_args()
 
 
-    response =  client.chat.completions.create(
-    model="openrouter/free",
     messages=[
         {
             "role": "user",
             "content": args.user_prompt,
         }
-    ],
+    ]
+
+
+    response =  client.chat.completions.create(
+    model="openrouter/free",
+    messages= messages,
 )
 
     if response.usage is None:
@@ -41,8 +45,12 @@ def main():
     prompt_token_nr = response.usage.prompt_tokens
     response_token_nr = response.usage.completion_tokens
 
-    print(f"Prompt tokens: {prompt_token_nr}")
-    print(f"Response tokens: {response_token_nr}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_token_nr}")
+        print(f"Response tokens: {response_token_nr}")
+
+
     print(response.choices[0].message.content)
 
 main()
